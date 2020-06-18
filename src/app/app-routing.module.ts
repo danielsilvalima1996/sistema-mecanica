b914c3e087096287148544a603612b1c7f70f5b6
danielsilvalima1996 <dsl15021996@gmail.com>
2020-06-18 12:43:01 -0300

    add router-outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'login', loadChildren: './login/login.module#LoginModule' },
+  // { path: 'login', loadChildren: './login/login.module#LoginModule' },
+  { path: '', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
   {
     path: 'ordem-servico', loadChildren:
-      () => import('./login/login.module').then(m => m.LoginModule)
+      () => import('./ordem-servico/ordem-servico.module').then(m => m.OrdemServicoModule)
   },
 ];
 
--- src/app/ordem-servico/ordem-servico.module.ts
+++ src/app/ordem-servico/ordem-servico.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrdemServicoListComponent } from './ordem-servico-list/ordem-servico-list.component';
+import { PoPageModule, PoTableModule, PoFieldModule, PoButtonModule } from '@po-ui/ng-components';
 
 
 
 @NgModule({
   declarations: [OrdemServicoListComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    PoPageModule,
+    PoTableModule,
+    PoFieldModule,
+    PoButtonModule
   ]
 })
 export class OrdemServicoModule { }
