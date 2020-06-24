47e18b3ac65eec64ed89dd2cce322ee75480ea17
aksilvaa <alison.keuver@gmail.con>
2020-06-24 12:37:54 -0300

    correcoes no modulo de cadastro e toolbar logout

---
+++ src/app/services/usuarios/usuarios.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { Users } from 'src/app/interfaces/users.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuariosService {
+
+  private relativeLink: string = `${environment.api}/users`;
+
+  constructor(private http: HttpClient) { }
+
+  public findAll(): Observable<Array<Users>> {
+    return this.http.get(`${this.relativeLink}/all`) as Observable<Array<Users>>;
+  }
+
+  public findById(id: any): Observable<Users> {
+    return this.http.get(`${this.relativeLink}/${id}`) as Observable<Users>;
+  }
+
+  public createUser(users: Users): Observable<Users> {
+    return this.http.post(`${this.relativeLink}`, users) as Observable<Users>;
+  }
+
+}
