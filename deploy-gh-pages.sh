4fe61bdaf3ecae92ad6fbc4dd82a8c8df0987973
danielsilvalima1996 <dsl15021996@gmail.com>
2020-11-03 11:45:10 -0300

    atualizado deploy

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -1,5 +1,5 @@
 # ng build --prod --base-href "/sistema-mecanica/"
 npm run ghp-build
 git add dist -f
-git commit -m "buiid gh pages adicionado dist"
+git commit -m "build gh pages adicionado dist"
 git subtree push --prefix dist origin gh-pages
