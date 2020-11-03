9ccce2e2c81b9555cdde92c56d02acf03b12be28
danielsilvalima1996 <dsl15021996@gmail.com>
2020-11-03 10:00:30 -0300

    alterado environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api: 'http://localhost:8081/api'
+  api: 'https://apirest-mecanica.herokuapp.com/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  api: 'http://localhost:8081/api'
+  api: 'https://apirest-mecanica.herokuapp.com/api'
 };
 
 /*
